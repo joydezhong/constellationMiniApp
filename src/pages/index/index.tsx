17aa04bf613660dde81ecb3eb7c6ac75b80685d6
zhongweipeng <zhongwp@shushangyun.com>
2021-09-22 18:08:59 +0800

    fix: 模拟数据

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,35 +1,70 @@
 import { useState } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { AtButton, AtDivider } from 'taro-ui'
 import './index.scss'
 
-const Index = () => {
-  const [toggle, setToggle] = useState(false)
+const data = [
+  {
+    id: 1,
+    title: '这是一段标题',
+    text: '这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。',
+    display: false,
+  },
+  {
+    id: 2,
+    title: '这是二段标题',
+    text: '这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。',
+    display: false,
+  },
+  {
+    id: 3,
+    title: '这是三段标题',
+    text: '这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。',
+    display: false,
+  }
+]
 
-  const handleExpand = () => {
-    setToggle(!toggle)
+const Index = () => {
+  const [source, setSource] = useState(data)
+
+  const handleExpand = (id) => {
+    const _data = [...source]
+    setSource(() => _data.map(item => {
+      if(item.id === id) {
+        return {
+          ...item,
+          display: true,
+        }
+      }
+      return item
+    }))
+
+  }
+
+  const handleControl = () => {
+    setSource(() => source.map(item => ({ ...item, display: false })))
   }
 
   return (
     <View className='index' style={{color: '000'}}>
-      <AtButton type='primary' size='normal'>按钮文案</AtButton>
-      <View className='at-article__h2'>这是二级标题</View>
-      <View className='at-article__h3'>这是三级标题</View>
-      <View className='at-article__p'>
-        <View className={toggle ? 'textBoxShow' : 'textBoxHide'}>
-          <Text>
-            这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。
-            这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。
-            这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。
-          </Text>
-          <View onClick={handleExpand} className={toggle ? "moreBoxHide" : "moreBoxShow"}>
-            <View className="description">
-              <Text>查看更多说明</Text>
-              <View className='at-icon at-icon-chevron-down'></View>
+      <AtButton type='primary' size='normal' onClick={handleControl}>按钮文案</AtButton>
+      {
+        source.map(item => (<View>
+          <View className='at-article__h3'>{item.title}</View>
+            <View className='at-article__p'>
+              <View className={item.display ? 'textBoxShow' : 'textBoxHide'}>
+                <Text>{item.text}</Text>
+                <View onClick={() => handleExpand(item.id)} className={item.display ? "moreBoxHide" : "moreBoxShow"}>
+                  <View className="description">
+                    <Text>查看更多说明</Text>
+                    <View className='at-icon at-icon-chevron-down'></View>
+                  </View>
+                </View>
+              </View>
             </View>
-          </View>
-        </View>
-      </View>
+          <AtDivider content='分割线' />
+        </View>))
+      }
     </View>
   )
 }
